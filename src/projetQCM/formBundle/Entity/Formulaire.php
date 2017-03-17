101246b9548e9e2e450ed3fa4287e425b423cfb8
Hemeuryc <clemeury@hotmail.fr>
2017-03-17 12:16:24 +0100

    ENtities ok
    entitées c fé

---
+++ src/projetQCM/formBundle/Entity/Formulaire.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace projetQCM\formBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Formulaire
+ *
+ * @ORM\Table(name="formulaire")
+ * @ORM\Entity(repositoryClass="projetQCM\formBundle\Repository\FormulaireRepository")
+ */
+class Formulaire
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+
+    /**
+     * @ORM\OneToMany(targetEntity="Questionnaire", mappedBy="formulaire")
+     */
+    private $questionnaires;
+
+    public function __construct()
+    {
+        $this->questionnaires = new ArrayCollection();
+    }
+
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Formulaire
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Add questionnaire
+     *
+     * @param \projetQCM\formBundle\Entity\Questionnaire $questionnaire
+     *
+     * @return Formulaire
+     */
+    public function addQuestionnaire(\projetQCM\formBundle\Entity\Questionnaire $questionnaire)
+    {
+        $this->questionnaires[] = $questionnaire;
+
+        return $this;
+    }
+
+    /**
+     * Remove questionnaire
+     *
+     * @param \projetQCM\formBundle\Entity\Questionnaire $questionnaire
+     */
+    public function removeQuestionnaire(\projetQCM\formBundle\Entity\Questionnaire $questionnaire)
+    {
+        $this->questionnaires->removeElement($questionnaire);
+    }
+
+    /**
+     * Get questionnaires
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getQuestionnaires()
+    {
+        return $this->questionnaires;
+    }
+}
