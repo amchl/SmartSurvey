5cf3aadaacb3b35c75d60035ca170ae0e16e9d5e
Jukelips <adrien.mchl@gmail.com>
2017-03-17 11:16:34 +0100

    Form pour la question
    commit

--- src/projetQCM/formBundle/Controller/FormController.php
+++ src/projetQCM/formBundle/Controller/FormController.php
@@ -16,4 +16,20 @@ class FormController extends Controller
     {
         return $this->render('formBundle:Form:form.html.twig');
     }
+
+    public function formAction()
+    {
+        $question = new Question();
+        $formBuilder = $this->get('form.Factory')->createBuilder(FormType::class, $question);
+
+        $formBuilder
+            ->add('question', TextType::class)
+            ->add('submit', SubmitType::class );
+
+        $form = $formBuilder->getForm();
+
+        return $this->render('formBundle:Form:qForm.html.twig', array(
+            'form'=>$form->createView()
+        ));
+    }
 }
