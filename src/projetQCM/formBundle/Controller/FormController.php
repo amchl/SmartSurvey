7e61412d75c9b7dbd24ceee818d55771c84a5ecf
Jukelips <adrien.mchl@gmail.com>
2017-04-04 19:12:58 +0200

    commit séparation form

--- src/projetQCM/formBundle/Controller/FormController.php
+++ src/projetQCM/formBundle/Controller/FormController.php
@@ -9,10 +9,12 @@
 namespace projetQCM\formBundle\Controller;
 
 use projetQCM\formBundle\Entity\Formulaire;
+use projetQCM\formBundle\Entity\Question;
 use projetQCM\formBundle\Form\FormulaireType;
 use projetQCM\formBundle\Form\QuestionType;
 use projetQCM\formBundle\Form\ReponseType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,12 +29,28 @@ class FormController extends Controller
     public function formAction(Request $request)
     {
         $formulaire = new Formulaire();
+        $question = new Question();
         $form = $this->get('form.factory')->createBuilder(FormulaireType::class, $formulaire)
             ->add('title', TextType::class)
             ->add('q', QuestionType::class)
             ->add('r', ReponseType::class)
+            //->add('+', ButtonType::class)
+
             ->add('envoyer', SubmitType::class)
             ->getForm();
+
+        //$request = $this->get('request');
+        /*if ($request->request->has('+'))
+        {
+            $form->add('r', ReponseType::class);
+        }*/
+        /*if ($form->isValid()) {
+            if ($form->get('+')->isClicked()) {
+                $form->add('r', ReponseType::class);
+            }
+        }        if ($form->get('+')->onClicked()) {
+            $form->add('r', TextType::class);
+        }*/
 
         if ($request->isMethod('POST')) {
             $form->handleRequest($request);
